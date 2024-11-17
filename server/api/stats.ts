c6a2d488e05c4316545712dcccc51d4190c8a254
Chew <chew@chew.pw>
2024-11-17 15:17:48 -0600

    feat: use database to grab stats

--- server/api/stats.ts
+++ server/api/stats.ts
@@ -1,14 +1,32 @@
 import type {Stats} from "~/types/stats";
+import prisma from "~/lib/prisma";
 
 export default defineEventHandler(async () => {
-    // Fetch the stats from the API
-    const res: Response = await fetch(`https://api.memerator.me/v1/stats`, {
-        headers: {
-            Authorization: useRuntimeConfig().memeratorApiKey,
-        }
-    })
+    // Get the results simultaneously
+    const results = await Promise.allSettled([
+        prisma.memes.count({
+            where: {
+                disabled: false,
+            },
+        }),
+        prisma.ratings.count(),
+        prisma.users.count(),
+        prisma.memes.groupBy({
+            by: ['user_id'],
+            _count: {
+                user_id: true,
+            },
+        }).then(results => results.length),
+    ]);
 
-    // json time
-    // Return the meme
-    return await res.json() as Stats
+    // Map to the stats object
+    const response: Stats = {
+        memes: results[0].status === 'fulfilled' ? results[0].value : 0,
+        ratings: results[1].status === 'fulfilled' ? results[1].value : 0,
+        website_users: results[2].status === 'fulfilled' ? results[2].value : 0,
+        unique_memers: results[3].status === 'fulfilled' ? results[3].value : 0,
+    };
+
+    // Return the stats
+    return response;
 })
